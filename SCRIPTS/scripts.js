2654a00a15033849787aefe0f5c273b740936b4a
jika026 <jiksboy2000@gmail.com>
2024-06-13 17:16:34 +0100

    first commit

---
+++ SCRIPTS/scripts.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector("header #hamburger");
+const dropdownMenu = document.getElementById("dropdownMenu");
+const nav = document.querySelector("nav");
+hamburger.addEventListener("click", () => {
+  dropdownMenu.classList.toggle("show");
+  if (hamburger.classList.contains("fa-bars")) {
+    hamburger.classList.remove("fa-bars");
+    hamburger.classList.add("fa-xmark");
+  } else {
+    hamburger.classList.remove("fa-xmark");
+    hamburger.classList.add("fa-bars");
+  }
+});
